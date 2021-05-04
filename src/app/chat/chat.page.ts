a29b9c83f5a3718493d5a5cede976b89aac1b4c9
Rishi <rishishankar23@gmail.com>
2021-05-03 20:24:35 -0400

    Integrated Jacob's sending code

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-chat',
@@ -9,12 +10,19 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class ChatPage implements OnInit {
 
   messages: any;
+  currMessage = {username:"", text:"", likes:0};
 
   constructor(private firestore: AngularFirestore) {
      this.messages = firestore.collection<any>("messages").valueChanges();
   }
 
   ngOnInit() {
+  }
+
+  addMessage() {
+  	var user1 = firebase.auth().currentUser;
+  	this.currMessage.username = user1.displayName;
+  	this.firestore.collection<any>("messages").add(this.currMessage);
   }
 
 }
