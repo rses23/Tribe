29f2fb6ef2b7f6874a45aa89b23f18a9cc2f4b2a
jacobletizia <jletizia@email.sc.edu>
2021-05-03 21:38:48 -0400

    added dates to messages
    also attempted to do something about the like button

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import firebase from 'firebase/app';
+import { Message } from '../modal/Message';
 
 @Component({
   selector: 'app-chat',
@@ -10,7 +11,7 @@ import firebase from 'firebase/app';
 export class ChatPage implements OnInit {
 
   messages: any;
-  currMessage = {username:"", text:"", likes:0};
+  currMessage:Message = {username:"", text:"", likes:0, timestamp:''};
 
   constructor(private firestore: AngularFirestore) {
      this.messages = firestore.collection<any>("messages").valueChanges();
@@ -22,7 +23,12 @@ export class ChatPage implements OnInit {
   addMessage() {
   	var user1 = firebase.auth().currentUser;
   	this.currMessage.username = user1.displayName;
+    this.currMessage.likes = 0;
+    this.currMessage.timestamp = new Date().toISOString;
   	this.firestore.collection<any>("messages").add(this.currMessage);
   }
 
+  like() {
+    console.log("like was clicked");
+  }
 }
---
+++ src/app/modal/Message.ts
@@ -0,0 +1,6 @@
+export interface Message {
+	text: string;
+	username: string;
+	timestamp: any;
+	likes:number;
+}
